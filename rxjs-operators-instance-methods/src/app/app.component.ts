210163636c62f08ffabd57b791f99e756ed2cfd0
Jhedie <jeddiahawuku12@gmail.com>
2022-12-28 23:26:13 +0000

    feat: Add useful instance operators

--- rxjs-operators-instance-methods/src/app/app.component.ts
+++ rxjs-operators-instance-methods/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, map, mergeScan, mergeWith, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,14 +20,22 @@ export class AppComponent {
     'The Matrix (1999)',
     'The Lord of the Rings (2001)',
   ];
-  outputStreamData: Number[] = [];
+  outputStreamData: String[] = [];
 
   startStream() {
-    const streamSource = interval(2000);
-    const moviesStream = interval(3000).pipe();
-    this.subscription = streamSource.subscribe((element) =>
-      this.outputStreamData.push(element)
+    const streamSource = interval(1000);
+    const moviesStreamSource = interval(2000).pipe(
+      map((value) => value % this.moviesStreamData.length),
+      map((index) => this.moviesStreamData[index])
     );
+    this.subscription = streamSource
+      .pipe(
+        map((value) => value % this.bookStreamData.length),
+        map((index) => this.bookStreamData[index]),
+        mergeWith(moviesStreamSource),
+        tap((element) => this.outputStreamData.push(element))
+      )
+      .subscribe();
   }
 
   stopStream() {
