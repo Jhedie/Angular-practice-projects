b02722507b38761e7f5e161e00a6eafca8ccd584
Jhedie <jeddiahawuku12@gmail.com>
2022-12-28 22:22:21 +0000

    feat: Initialise project

--- rxjs-operators-instance-methods/src/app/app.component.ts
+++ rxjs-operators-instance-methods/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rxjs-operators-instance-methods';
+  title = 'Working with RxJS operators using instance methods';
 }
