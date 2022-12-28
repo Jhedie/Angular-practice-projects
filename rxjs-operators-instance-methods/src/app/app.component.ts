24c1977665efbc8341a45b143de61f27be8f7669
Jhedie <jeddiahawuku12@gmail.com>
2022-12-28 23:15:46 +0000

    feat: Add stream boxes and input stream data
    Add styling for page
    
    Add rxjs icon

--- rxjs-operators-instance-methods/src/app/app.component.ts
+++ rxjs-operators-instance-methods/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Working with RxJS operators using instance methods';
+  subscription: Subscription | null = null;
+  bookStreamData: String[] = [
+    'The Great Gatsby',
+    'The Lord of the Rings',
+    'War and Peace',
+    'Alices Adventures in Wonderland',
+  ];
+  moviesStreamData: String[] = [
+    'Avatar: The Way of Water (2022)',
+    'The Matrix (1999)',
+    'The Lord of the Rings (2001)',
+  ];
+  outputStreamData: Number[] = [];
+
+  startStream() {
+    const streamSource = interval(2000);
+    const moviesStream = interval(3000).pipe();
+    this.subscription = streamSource.subscribe((element) =>
+      this.outputStreamData.push(element)
+    );
+  }
+
+  stopStream() {
+    this.subscription?.unsubscribe();
+    this.subscription = null;
+  }
 }
