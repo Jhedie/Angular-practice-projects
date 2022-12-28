446dbf3c9c9c01728a39c9d63e15aba0b9917555
Jhedie <jeddiahawuku12@gmail.com>
2022-12-28 23:42:47 +0000

    feat: Add bootstrap cdn
    Add three stream boxes
    
    Add styling
    
    Initialise AppComponent methods and template variables

--- rxjs-operators-static-methods/src/app/app.component.ts
+++ rxjs-operators-static-methods/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rxjs-operators-static-methods';
+  title = 'Working with RxJs operators using static methods';
+  subscription: any;
+  startStream() {
+    throw new Error('Method not implemented.');
+  }
+  stopStream() {
+    throw new Error('Method not implemented.');
+  }
 }
