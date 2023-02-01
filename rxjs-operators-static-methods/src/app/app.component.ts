9becc2570357baef65261945a6bdb2c467dabcd2
Jhedie <jeddiahawuku12@gmail.com>
2023-02-01 01:14:51 +0000

    feat: Implement start and stop stream functions
    Allow stream data to be passed into output boxes when stream starts

--- rxjs-operators-static-methods/src/app/app.component.ts
+++ rxjs-operators-static-methods/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { interval, map, tap } from 'rxjs';
 
+interface streamData {
+  type: string;
+  title: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +13,47 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Working with RxJs operators using static methods';
   subscription: any;
+  outputStream: streamData[] = [];
+  combinedStreamData: streamData[] = [
+    {
+      type: 'country',
+      title: 'India',
+    },
+    {
+      type: 'bird',
+      title: 'Peacock',
+    },
+    {
+      type: 'country',
+      title: 'Japan',
+    },
+    {
+      type: 'bird',
+      title: 'Eagle',
+    },
+    {
+      type: 'bird',
+      title: 'Penguin',
+    },
+    {
+      type: 'country',
+      title: 'France',
+    },
+  ];
+
   startStream() {
-    throw new Error('Method not implemented.');
+    const streamSource = interval(3000).pipe(
+      map((input) => {
+        const index = input % this.combinedStreamData.length;
+        return this.combinedStreamData[index];
+      })
+    );
+    this.subscription = streamSource
+      .pipe(tap((value) => this.outputStream.push(value)))
+      .subscribe();
   }
   stopStream() {
-    throw new Error('Method not implemented.');
+    this.subscription.unsubscribe();
+    this.subscription = null;
   }
 }
