a045026dd20c3ff44d1bac7b501197650eb8b404
Jhedie <jeddiahawuku12@gmail.com>
2023-02-16 18:27:16 +0000

    feat: Initialise cc-setters practice project

---
+++ cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-vc-logs',
+  templateUrl: './vc-logs.component.html',
+  styleUrls: ['./vc-logs.component.scss'],
+})
+export class VcLogsComponent implements OnInit {
+  @Input() vName;
+  logs: string[] = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
