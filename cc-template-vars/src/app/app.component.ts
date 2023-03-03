23643970c92b5d3a8fe86d57870029ad4b29407a
JedidiahAwuku <jedidiah.awuku@sap.com>
2023-03-03 15:48:04 +0000

    feat: Allow adding and removing of pictures

--- cc-template-vars/src/app/app.component.ts
+++ cc-template-vars/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cc-template-vars';
-
-  addNewPicture() {
+  // With the template reference variable I am able to reference the gallery component methods and other properties
+  addNewPicture(gallery: GalleryComponent) {
+    gallery.pictures.unshift(gallery.generateImage());
     console.log('added new picture');
   }
 
-  removeFirstPicture() {
+  removeFirstPicture(gallery: GalleryComponent) {
+    gallery.pictures.shift();
     console.log('removed first picture');
   }
 }
