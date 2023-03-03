fa9e3fe0b8d7f68dfd95f06a009bae702d8e657d
JedidiahAwuku <jedidiah.awuku@sap.com>
2023-03-03 13:09:21 +0000

    feat: Use @Input getters and setters for sematics version logs

--- cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -12,6 +12,12 @@ export class VcLogsComponent implements OnInit {
     return this._vName;
   }
   set vName(name: string) {
+    if (!name) return;
+    if (!this._vName) {
+      this.logs.push(`initial version is ${name.trim()}`);
+    } else {
+      this.logs.push(`version changed to ${name.trim()}`);
+    }
     this._vName = name;
   }
 
