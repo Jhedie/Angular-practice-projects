ec12054011a7e276a59f5bd7471d87b0ddf72313
Jhedie <jeddiahawuku12@gmail.com>
2023-05-10 12:08:23 +0100

    feat:Add standard input output mechanism for slider value

--- control-value-accessors/src/app/app.component.ts
+++ control-value-accessors/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'control-value-accessors';
+  sliderValue: any = 50;
+
+  onSliderValueChange($event: any) {
+    console.log('$event', $event);
+    this.sliderValue = $event;
+  }
 }
--- control-value-accessors/src/app/components/ngx-jquery-slider/ngx-jquery-slider.component.ts
+++ control-value-accessors/src/app/components/ngx-jquery-slider/ngx-jquery-slider.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 declare const $: any;
 
@@ -9,10 +17,25 @@ declare const $: any;
 })
 export class NgxJquerySliderComponent implements AfterViewInit {
   @ViewChild('location') location: any;
+  @Input() value: any;
+  @Output() private valueChange: EventEmitter<any> = new EventEmitter();
   widget: any;
 
   constructor() {}
   ngAfterViewInit(): void {
     this.widget = $(this.location.nativeElement).slider();
+    //this sets the widget to the value passed in when the component view is initialized
+    this.widget.slider('value', this.value);
+    // after action slidestop-when the user stops moving the slider, emit the new value
+    this.widget.on('slidestop', (event: any, ui: { value: any }) => {
+      this.valueChange.emit(ui.value);
+    });
+  }
+
+  ngOnChanges() {
+    //if the widget exists and the value is not equal to the value of the widget, set the value of the widget
+    if (this.widget && this.widget.slider('value') !== this.value) {
+      this.widget.slider('value', this.value);
+    }
   }
 }
