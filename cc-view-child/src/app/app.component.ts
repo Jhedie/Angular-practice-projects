0d616d2c132887746c9fb504ee1d550cb6f63610
Jhedie <jeddiahawuku12@gmail.com>
2023-05-10 10:55:46 +0100

    feat(cc-view-child): Allow adding and removing of pictures

--- cc-view-child/src/app/app.component.ts
+++ cc-view-child/src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cc-view-child';
+  /*
+
+    1. ViewChild is a decorator that the angular/core package provides out the box
+    2. It configures a 'view query' for the Angular change detector
+
+    */
+  @ViewChild(GalleryComponent) gallery: GalleryComponent;
 
   addNewPicture() {
     console.log('added new picture');
+    this.gallery.pictures.unshift(this.gallery.generateImage());
   }
 
   removeFirstPicture() {
     console.log('removed first picture');
+    this.gallery.pictures.shift();
   }
 }
