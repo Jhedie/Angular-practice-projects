9a767589bb8e442d5473d84fb370fe91ad63a269
Jhedie <jeddiahawuku12@gmail.com>
2023-05-11 12:54:22 +0100

    chore: Add helpful comments to code

--- ad-attribute-directive/src/app/directives/highlight.directive.ts
+++ ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -15,19 +15,29 @@ export class HighlightDirective implements OnChanges {
   originalHTML: string = '';
 
   constructor(private el: ElementRef) {}
+
   ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes.HighlightText', changes.highlightText);
+
+    // SimpleChanges as a parameter, which contains information about the changes to the input properties.
+
+    //when no changes have been made - firstChange is true then store the original html
     if (changes.highlightText.firstChange) {
       this.originalHTML = this.el.nativeElement.innerHTML;
       return;
     }
+
     const { currentValue } = changes.highlightText;
+    console.log('currentValue', currentValue);
     if (currentValue) {
+      //g means global, i means case insensitive
       const regExp = new RegExp(`(${currentValue})`, 'gi');
       this.el.nativeElement.innerHTML = this.originalHTML.replace(
         regExp,
         `<span style="background-color: ${this.hightlightColor}">\$1</span>`
       );
     } else {
+      // if no value, reset to original html
       this.el.nativeElement.innerHTML = this.originalHTML;
     }
   }
